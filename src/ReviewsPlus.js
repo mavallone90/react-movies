f3a9db9f3eca7177b2c1c633097c23b445d95052
MLA <mla@MLAs-MBP.fios-router.home>
2021-06-02 22:46:30 -0400

    got reviewsPlus working better

--- src/ReviewsPlus.js
+++ src/ReviewsPlus.js
@@ -3,7 +3,11 @@ import { tmdb_apikey } from "./consts";
 
 const ReviewsPlus = () => {
   const [review, setReview] = React.useState([]);
-  const [reviewPlus, setReviewPlus] = React.useState([]);
+  React.useEffect(() => {
+    fetch("./allReviews.json")
+      .then((res) => res.json())
+      .then((data) => setReview(data));
+  }, []);
 
   var getID = async function (movie, year) {
     const idReq =
@@ -18,7 +22,7 @@ const ReviewsPlus = () => {
       .then((response) => response.json())
       .then((data) => data.results[0].id)
       .catch(function (error) {
-        console.log(error);
+        console.log("broken ids.");
       });
 
     const ans = await Promise.resolve(filmID);
@@ -31,68 +35,51 @@ const ReviewsPlus = () => {
     return ans;
   };
 
-  React.useEffect(() => {
-    fetch("./allReviews.json")
-      .then((res) => res.json())
-      .then((data) => setReview(data));
-  }, []);
-
   // getID("Snatch", "2000");
-  // var testArray = [{ Hi: "Ho", Said: "Hey" }];
-  // console.log(testArray);
+
+  const [reviewPlus, setReviewPlus] = React.useState([]);
 
   React.useEffect(() => {
-    var idArray = [];
+    // var idArray = { i: [] };
 
     review.results &&
       review.results.map(async (rev) => {
-        if (rev.Rating === 5) {
-          // return console.log(getID(rev.Name, rev.Year));
-          return idArray.push(
-            // [{ Item: await getID(rev.Name, rev.Year), Other: "Thing" }]
-            [
-              {
-                id: await getID(rev.Name, rev.Year),
-                title: rev.Name,
-                year: rev.Year,
-                rating10: rev.Rating * 2,
-                theReview: rev.Review,
-                lb_link: rev["Letterboxd URI"],
-                local_link:
-                  "http://localhost:3000/OneFilm/" +
-                  (await getID(rev.Name, rev.Year)),
-              },
-            ]
-          );
-          // rev.Name + " " + rev.Year,
+        if (rev.Rating === 5 || rev.Rating === 4.5) {
+          reviewPlus.push({
+            id: await getID(rev.Name, rev.Year),
+            title: rev.Name,
+            year: rev.Year,
+            rating10: rev.Rating * 2,
+            theReview: rev.Review,
+            lb_link: rev["Letterboxd URI"],
+            // local_link:
+            //   "http://localhost:3000/OneFilm/" +
+            //   (await getID(rev.Name, rev.Year)),
+          });
         } else return [];
       });
 
     // console.log(idArray);
-    setReviewPlus(idArray);
-  }, [review.results]);
-
-  console.log(reviewPlus[0]);
+    setReviewPlus(reviewPlus);
+  }, [review.results, reviewPlus]);
 
   return (
     <div style={{ paddingLeft: "20px" }}>
-      <h2> Perfect Movies:</h2>
-      {/* <p>{idArray.toString()}</p> */}
-      {/* <ul>
-        {review.results &&
-          review.results.map((rev) => {
-            if (rev.Rating === 5) {
-              return (
-                <a href={rev["Letterboxd URI"]}>
-                  <li>
-                    {rev.Name} {rev.Year}
-                    <li>And this</li>
-                  </li>
-                </a>
-              );
-            } else return [];
+      <h2> Great Fucking Movies:</h2>
+      <ul>
+        {reviewPlus &&
+          reviewPlus.map((rev) => {
+            return (
+              <li key={rev.title}>
+                {rev.title} ({rev.year}){" "}
+                <em style={{ color: "red" }}>
+                  {rev.id ? "" : "BROKE!  FIX THIS SHIT!"}
+                </em>
+              </li>
+              // console.log("A Movie:", rev.title, "// The ID:", rev.id)
+            );
           })}
-      </ul> */}
+      </ul>
     </div>
   );
 };
